b7ca493a63f64d1a1a2d3f5e4ceff308e2861c69
heyhey1028 <shoheiogawa1028@gmail.com>
2022-10-18 12:30:52 +0900

    update sample_screen for toybox_template

--- bricks/toybox_template/__brick__/{{name}}/lib/features/sample/sample_screen.dart
+++ bricks/toybox_template/__brick__/{{name}}/lib/features/sample/sample_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_toybox/widgets/app_scaffold.dart';
 
 class SampleScreen extends ConsumerWidget {
   const SampleScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return AppScaffold(
+      body: const Center(
+        child: Text('Sample page'),
+      ),
+    );
   }
 }
